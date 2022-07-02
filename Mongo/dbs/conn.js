4b9065c41f32ca825c0bf85673b36909bc59ec16
Sailee Kadam <sailee.kadam19@vit.edu>
2022-07-02 14:02:41 +0530

    exported local db to json files

--- Mongo/dbs/conn.js
+++ Mongo/dbs/conn.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/freelance",{
+const local = "mongodb://localhost:27017/freelance";
+const hosted = "mongodb+srv://freelance_services:freelance_services@cluster0.gyndj.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(hosted,{
     useNewUrlParser : true,
 }).then(() => {
     console.log("Database Connected..!")
